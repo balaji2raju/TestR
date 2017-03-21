b477f6d1e7fef7205465e080e0ca7fb828d0b19d
balaji raju <balaji2raju@gmail.com>
2017-03-21 11:27:11 +0530

    Commiting 2nd attempt

--- JVP/src/com/test/espn/Samp.java
+++ JVP/src/com/test/espn/Samp.java
@@ -4,7 +4,7 @@ public class Samp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("I am in Sample class.....");
+System.out.println("I am in Sample class.....belongs to Parent....");
 	}
 
 }
